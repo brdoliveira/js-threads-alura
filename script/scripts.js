5c33bb0d5a7538e6dc27ec8e8bfcaffc0d5f3d75
Bruno Ribeiro <bruno.oliveira@bandtec.com.br>
2023-07-19 11:41:42 -0300

    REFACT(script): Movendo responsabilidade de requisição para Worker

--- script/scripts.js
+++ script/scripts.js
@@ -13,17 +13,6 @@ const graficoParaDolar = new Chart(graficoDolar, {
     },
 });
 
-async function conectaAPI() {
-    const conecta = await fetch("https://economia.awesomeapi.com.br/json/last/USD-BRL");
-    const conectaTraduzido = await conecta.json();
-    let tempo = geraHorario();
-    let valor = conectaTraduzido.USDBRL.ask;
-    adicionarDados(graficoParaDolar, tempo, valor);
-    imprimeCotacao("dolar", valor)
-}
-
-setInterval(() => conectaAPI(), 5000)
-
 function geraHorario() {
     let data = new Date();
     let horario = data.getHours() + ":" + data.getMinutes() + ":" + data.getSeconds();
@@ -37,3 +26,13 @@ function adicionarDados(grafico, legenda, dados) {
     })
     grafico.update();
 }
+
+let workerDolar = new Worker('./script/workers/workerDolar.js');
+workerDolar.postMessage('usd');
+
+workerDolar.addEventListener("message", event => {
+    let tempo = geraHorario();
+    let valor = event.data.ask;
+    imprimeCotacao("dolar", valor);
+    adicionarDados(graficoParaDolar, tempo, valor);
+})
